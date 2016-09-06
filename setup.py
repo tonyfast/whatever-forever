1db7e7809417735d0ba2e73b04c9434cb0951290
tonyfast <tony.fast@gmail.com>
2016-09-06 10:10:47 -0400

    Release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setuptools.setup(
     name="whatever-forever",
-    version="0.1.1",
+    version="0.1.2",
     author="Tony Fast",
     author_email="tony.fast@gmail.com",
     description="prototype whatever in the Jupyter notebook",
--- whatever/__init__.py
+++ whatever/__init__.py
@@ -5,7 +5,7 @@
 from .chain import Chain, this, _X, _P
 from . import callables
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = [
     'this', 'callables', '_X', '_P', 'Chain',
