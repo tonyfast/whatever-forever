9c4813541a2d6372c951da8ec9fe9e0220bfa5fa
tonyfast <tony.fast@gmail.com>
2016-09-07 10:05:12 -0400

    Trying releasing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setuptools.setup(
     name="whatever-forever",
-    version="0.1.2",
+    version="0.1.3",
     author="Tony Fast",
     author_email="tony.fast@gmail.com",
     description="prototype whatever in the Jupyter notebook",
--- whatever/__init__.py
+++ whatever/__init__.py
@@ -2,10 +2,10 @@
 
 # In[ ]:
 
-from .chain import Chain, this, _X, _P
-from . import callables
+from whatever.chain import Chain, this, _X, _P
+from whatever import callables
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = [
     'this', 'callables', '_X', '_P', 'Chain',
